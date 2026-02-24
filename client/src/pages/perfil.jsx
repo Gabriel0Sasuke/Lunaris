53062ba9a4bc0cea65b82f875705a235ef036883
gabriexlss <gabrielsasukeclash@gmail.com>
2026-02-24 14:41:12 -0300

    Inicio do Front do profile

--- client/src/pages/perfil.jsx
+++ client/src/pages/perfil.jsx
@@ -1,7 +1,18 @@
+import '../assets/styles/perfil.css';
+
 function Perfil(){
     return(
-        <main>
-            <h1>Perfil</h1>
+        <main className="perfil-content">
+            <div className="profileInfo">
+                <div className="profileIcon">
+                    <img src="/example/lunaChan.png" alt="Sua foto de perfil" />
+                    <div className="profileLevel">LVL 6</div>
+                </div>
+                <div className="profileName">
+                    <h2>Gabriel0Sasuke</h2>
+                </div>
+                <div className="profileTitle">Mangá Reader</div>
+            </div>
         </main>
     )
 }
